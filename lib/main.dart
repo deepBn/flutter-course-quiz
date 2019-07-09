381a7ef9aba21edba2e7f45ae4e83f9ad69db92f
Rajdeep <deepBn9@gmail.com>
2019-07-09 12:08:52 +0530

    added button options

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Question Answered!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color',
+      'What is your favourite animal'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is a default text'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
